1952dcd6023c92319b6355183a667a97f5e5f3fe
Bernhard Nebel <nebel@hinterm-ziel.de>
2026-01-24 23:33:59 +0100

    v3.0.4-pre1

--- Boards_manager_release.sh
+++ Boards_manager_release.sh
@@ -131,5 +131,5 @@ jq -r                                    \
 }' "package_${REALAUTHOR}_${REPOSITORY}_index.json.tmp" > "package_${REALAUTHOR}_${REPOSITORY}_index.json"
 
 # Remove files that's no longer needed
-rm -rf "$REPOSITORY-${DOWNLOADED_FILE#"v"}" "package_${AUTHOR}_${REPOSITORY}_index.json.tmp"
+rm -rf "$REPOSITORY-${DOWNLOADED_FILE#"v"}" "package_${REALAUTHOR}_${REPOSITORY}_index.json.tmp"
 
